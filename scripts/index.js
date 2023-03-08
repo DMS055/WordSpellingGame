6b3eb60e30036aea7c0459d0b573df6edfe6909d
passagenoir <60157796+DMS055@users.noreply.github.com>
2023-03-08 23:06:00 +0100

    Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let timer = document.getElementsByClassName("timer-div")[0];
 let streak = document.querySelector(".streak-div");
 let quizContainer = document.getElementById("container");
+let displayContainer = document.getElementById("display-container");
 let userRes = document.getElementById("user-res");
 let startScreen = document.querySelector(".start-screen");
 let startBtn = document.getElementById("start-btn");
@@ -8,3 +9,12 @@ let questionCount;
 let countdown;
 let score = 0;
 let count = 0;
+
+
+import { correctWords, incorrectWords, randomValue } from "/scripts/words.js";
+let wordArray = [];
+
+displayNext = () => {
+    questionCount += 1;
+    create();
+}
